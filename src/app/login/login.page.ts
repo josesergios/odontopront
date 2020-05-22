6774b8bf2d8ca377448aa843a4ee3a1ab630e6de
Sergio Santos <josesergio.s@hotmail.com>
2020-05-21 22:58:48 -0300

    request de autenticação no login sem funcionar - Erro de CORS

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,18 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { NavController } from '@ionic/angular';
+import { environment } from '../../environments/environment';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  export class LoginPage implements OnInit {
+    public itens:any;
+    constructor(private httpService: HttpClient) {
+    }
   
+    logindata:any ={};
+  
+    ngOnInit() {
+    }
 
+    login(){
+     let url="http://localhost:8000";
+     let data= {email:"user@odontopront.io", senha:"12345678"};
+     const requestOptions = {
+      headers: {
+        'Content-Type': 'application/json',
+        'Cache-Control': 'no-cache, private'
+      },
+      params: new HttpParams()
+    };
+    
+     this.httpService.post( url+'/login', data, requestOptions).toPromise().then(res => {
+       console.log("RESPOSTA", res);
+     });
+    }
 }
