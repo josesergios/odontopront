64bbec5f9d53cb9959ade8ea28b75a546e540bdc
Fred Lima <fredlima.dev@gmail.com>
2020-05-23 17:49:03 -0300

    Navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,13 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+  navigate : any;
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
   ) {
+    this.sideMenu();
     this.initializeApp();
   }
 
@@ -23,5 +25,32 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  sideMenu()
+  {
+    this.navigate =
+      [
+        {
+          title : "Pacientes",
+          url   : "/patients",
+          icon  : "finger-print-outline"
+        },
+        {
+          title : "Prontuários",
+          url   : "/medical-report",
+          icon  : "reader-outline"
+        },
+        {
+          title : "Usuários",
+          url   : "/users",
+          icon  : "people-outline"
+        },
+        {
+          title : "Perfil",
+          url   : "/profile",
+          icon  : "person-outline"
+        },
+      ]
   }
 }
