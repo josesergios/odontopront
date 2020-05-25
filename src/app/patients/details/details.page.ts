4fb7449a05bb864ab0d719e6c57dd871f7b1c41a
Sergio Santos <josesergio.s@hotmail.com>
2020-05-24 21:45:49 -0300

    page details - estático

--- src/app/patients/details/details.page.ts
+++ src/app/patients/details/details.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { ActivatedRoute } from '@angular/router';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 import { LOCAL_STORAGE, StorageService } from 'ngx-webstorage-service';
 
@@ -12,33 +12,54 @@ import { LOCAL_STORAGE, StorageService } from 'ngx-webstorage-service';
 })
 export class DetailsPage implements OnInit {
 
-  public patients: any;
+  //public patients: any;
+  public contato: Array<object> = [];
 
   constructor( private navCtrl: NavController, private httpClient: HttpClient, private httpRapams: HttpParams,
     private route: ActivatedRoute,
-    @Inject(LOCAL_STORAGE) private storage: StorageService) { }
+    @Inject(LOCAL_STORAGE) private storage: StorageService) {
+      this.contato = [
+        {
+        "name" : "Sergio",
+        "document": "01258862",
+        "birth_date": "20/05/2020",
+        "gender": "Masculino",
+        "skin_color": "Pardo",
+        "birth_city": "Feira de santana",
+        "birth_uf": "BA",
+        "marital_status": "Solteiro",
+        "mobile":"75 99988-9885",
+        "occupation": " Tec.suporte",
+        "address": {
+          "street": " Carneiro",
+          "number": "256",
+          "complement": "predio azul",
+          "neighborhood": "queimadinha",
+          "city": "Feira de santana",
+          "uf": "BA",
+          "postcode": "44050098",
+          "phone": "75 988996953"
+        }
+
+        }
+      ]
+     }
   
   async ngOnInit() {
-    // Esse método aqui é um método padrão pra toda página.
-    // Quando a página é renderizada, o que tem dentro desse método é executado, então, é aqui que vc vai 
-    // fazer a chamda da api ... tu não tá consumindo nada da api, por isso não tá mostrando nada.
-    // E outra, eu adicionei estático lá em cima , por isso quando clico em qualquer um dos paciente, aparece os mesmo dados
-    // deu pra entender ?
 
-    const requestOptions = {
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Authorization': 'Bearer ' + await this.storage.get('auth.token') // REveer isso amanhã, beleza ? 
-      },
-      params: new HttpParams()
-    };
+    // Isso aqui deve funcionar, vai trazer os dados dinâmicos ... funfou não kkk , deixa estático por enquanto ...
+    // const httpOptions = {
+    //   headers: new HttpHeaders({
+    //     'Content-Type':  'application/json',
+    //     'Authorization': 'Bearer ' + this.storage.get('auth.token')
+    //   })
+    // };
 
-    let id = this.route.snapshot.paramMap.get('id');
+    // let id = this.route.snapshot.paramMap.get('id');
 
-    this.httpClient.get(environment.baseurl + '/patients/' + id, requestOptions).toPromise().then(res => {
-      this.patients = res;
-    })
+    // this.httpClient.get(environment.baseurl + '/patients/' + id, httpOptions).toPromise().then(res => {
+    //  console.log('patients', res);
+    // })
   }
 
   goToBack(){
