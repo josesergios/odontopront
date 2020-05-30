2453cc7a2298c78f9fe30ac829d85c78bbcadbfa
Fred Lima <fredlima.dev@gmail.com>
2020-05-30 15:52:28 -0300

    Record details

--- src/app/records/details/details.page.ts
+++ src/app/records/details/details.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import {ActionSheetController, NavController} from '@ionic/angular';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
@@ -13,24 +13,51 @@ import { LOCAL_STORAGE, StorageService } from 'ngx-webstorage-service';
 export class DetailsPage implements OnInit {
   record : any;
   
-  constructor( private navCtrl: NavController, private httpClient: HttpClient,
+  constructor(
+    private navCtrl: NavController,
+    private httpClient: HttpClient,
     private route: ActivatedRoute,
-    @Inject(LOCAL_STORAGE) private storage: StorageService) { }
+    public actionSheetController: ActionSheetController,
+    @Inject(LOCAL_STORAGE) private storage: StorageService
+  ) { }
 
-    async ngOnInit() {
-      const httpOptions = {
-        headers: new HttpHeaders({
-          'Content-Type':  'application/json',
-          'Authorization': 'Bearer ' + await this.storage.get('auth.token')
-        })
-      };
-  
-      let id = this.route.snapshot.paramMap.get('id');
-
-      this.httpClient.get(environment.apiurl + '/records/' + id, httpOptions).toPromise().then(response => {
-        this.record = response;
+  async ngOnInit() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'Bearer ' + await this.storage.get('auth.token')
       })
-    }
+    };
+
+    let id = this.route.snapshot.paramMap.get('id');
+
+    this.httpClient.get(environment.apiurl + '/records/' + id, httpOptions).toPromise().then(response => {
+      this.record = response;
+    })
+  }
+
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Ações',
+      buttons: [{
+        text: 'Editar',
+        handler: () => {
+          console.log('Edit clicked');
+        }
+      }, {
+        text: 'Adicionar aferição de tensão arterial',
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Adicionar procedimento',
+        handler: () => {
+          console.log('Play clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+  }
 
   goToBack(){
     this.navCtrl.navigateBack('/records-list');
