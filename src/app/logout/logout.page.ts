8b5dd77446d0680e34f839fbf10d36c9ee230237
Fred Lima <fredlima.dev@gmail.com>
2020-05-30 10:22:39 -0300

    Logout

--- src/app/logout/logout.page.ts
+++ src/app/logout/logout.page.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, Injectable, OnInit} from '@angular/core';
+import {NavController} from "@ionic/angular";
+import {Router} from "@angular/router";
+import {LOCAL_STORAGE, StorageService} from "ngx-webstorage-service";
 
 @Component({
   selector: 'app-logout',
   templateUrl: './logout.page.html',
   styleUrls: ['./logout.page.scss'],
 })
+
+@Injectable()
 export class LogoutPage implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private navCtrl: NavController,
+    private router: Router,
+    @Inject(LOCAL_STORAGE) private storage: StorageService) {
   }
 
+  ngOnInit() {
+    this.storage.set('auth.user', '');
+    this.storage.set('auth.token', '');
+
+    this.router.navigateByUrl('/login');
+  }
 }
